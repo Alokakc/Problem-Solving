7b3fe7df44e67bd63e32fcbcfe035a581c2d79e7
Alok Kumar <121576359+Alokakc@users.noreply.github.com>
2024-05-05 03:41:25 +0530

    Update Solving_Questions_With_Brainpower.java

--- Online_TT_Class/Dynamic Programming-1/Solving_Questions_With_Brainpower.java
+++ Online_TT_Class/Dynamic Programming-1/Solving_Questions_With_Brainpower.java
@@ -1,3 +1,21 @@
+//memo
+class Solution {
+    public long solve(int i, int[][] q, long[] dp) {
+        if(i>=q.length) return 0;
+        if(dp[i] != -1) return dp[i];
+        long pick = q[i][0] + solve(i+q[i][1]+1, q, dp);
+        long skip = solve(i+1, q, dp);
+        return dp[i] = Math.max(pick, skip);
+    }
+    public long mostPoints(int[][] q) {
+        int n = q.length;
+        long[] dp = new long[n+1];
+        Arrays.fill(dp, -1);
+        return solve(0, q, dp);
+    }
+}
+
+//tabulation
 class Solution {
     public long mostPoints(int[][] q) {
         int n = q.length;
