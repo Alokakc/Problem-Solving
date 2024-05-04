a9ce6c93c64f64c401c0f6bf4119e1ae3306da81
Alok Kumar <121576359+Alokakc@users.noreply.github.com>
2024-05-05 03:37:14 +0530

    Update Climbing_Stairs.java

--- Online_TT_Class/Dynamic Programming-1/Climbing_Stairs.java
+++ Online_TT_Class/Dynamic Programming-1/Climbing_Stairs.java
@@ -1,3 +1,21 @@
+//memo
+class Solution {
+    public int solve(int n, int i, List<Integer> dp) {
+        if(i==n) return 1;
+        if(i>n) return 0;
+        if(dp.get(i) != -1) return dp.get(i);
+        int step1 = solve(n, i+1, dp);
+        int step2 = solve(n, i+2, dp);
+        dp.set(i, step1 + step2);
+        return step1 + step2;
+    }
+    public int climbStairs(int n) {
+        List<Integer> dp = new ArrayList(Collections.nCopies(n+1, -1));
+        return solve(n, 0, dp);
+    }
+}
+
+//tabulation
 class Solution {
     public int climbStairs(int n) {
         if(n==1) return 1;
@@ -10,3 +28,5 @@ class Solution {
         return dp[n];
     }
 }
+
+
