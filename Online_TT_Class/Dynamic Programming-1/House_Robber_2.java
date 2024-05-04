4f51e50736a21a69df0d02a8e9d6763249b633e5
Alok Kumar <121576359+Alokakc@users.noreply.github.com>
2024-05-05 03:39:10 +0530

    Update House_Robber_2.java

--- Online_TT_Class/Dynamic Programming-1/House_Robber_2.java
+++ Online_TT_Class/Dynamic Programming-1/House_Robber_2.java
@@ -1,3 +1,30 @@
+//memo
+class Solution {
+    int start = 0;
+    public int solve(int[] nums, int i, int[] dp) {
+        if(start == 0) {
+            if(i >= nums.length-1) return 0;
+        } else {
+            if(i >= nums.length) return 0;
+        }
+        if(dp[i] != -1) return dp[i];
+        int a = nums[i] + solve(nums, i+2, dp);
+        int b = solve(nums, i+1, dp);
+        return dp[i] = Math.max(a, b);
+    }
+    public int rob(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        int[] dp = new int[nums.length+1];
+        Arrays.fill(dp, -1);
+        int opt1 = solve(nums, 0, dp);
+        start = 1;
+        Arrays.fill(dp, -1);
+        int opt2 = solve(nums, 1, dp);
+        return Math.max(opt1, opt2);
+    }
+}
+
+//tabulation
 class Solution {
     public int rob2(int[] nums) {
         int n = nums.length;
