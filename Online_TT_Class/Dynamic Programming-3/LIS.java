980c204f98fe75cfbe09d711b0d0c16a028dcddd
Alok Kumar <121576359+Alokakc@users.noreply.github.com>
2024-05-07 02:45:35 +0530

    Update LIS.java

--- Online_TT_Class/Dynamic Programming-3/LIS.java
+++ Online_TT_Class/Dynamic Programming-3/LIS.java
@@ -1,3 +1,4 @@
+//memo
 import java.util.Arrays;
 
 class Solution {
@@ -18,5 +19,56 @@ class Solution {
             Arrays.fill(dp[i], -1);
         }
         return solve(0, -1, nums, dp);
+    }
+}
+
+//tabulation
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n+1][n+1];
+        for(int i=n-1; i>=0; i--) {
+            for(int prev=i-1; prev>=-1; prev--) {
+                int pick = Integer.MIN_VALUE;
+                if(prev == -1 || nums[i]>nums[prev]) {
+                    pick = 1 + dp[i+1][i+1];
+                }
+                int nonpick = dp[i+1][prev+1];
+                dp[i][prev+1] = Math.max(pick, nonpick);
+            }
+        }
+        return dp[0][0];
+    }
+}
+
+//Binary Search
+class Solution {
+    public int lower_bound(int n, List<Integer> nums, int target) {
+        int low = 0, high = n-1;
+        int ans = n;
+        while(low <= high) {
+            int mid = (low+high)/2;
+            if(nums.get(mid) >= target) {
+                ans = mid;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return ans;
+    }
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        List<Integer> sub = new ArrayList<>();
+        sub.add(nums[0]);
+        for(int i=1; i<n; i++) {
+            if(nums[i] > sub.get(sub.size()-1)) {
+                sub.add(nums[i]);
+            }
+            else {
+                int index = lower_bound(sub.size(), sub, nums[i]);
+                sub.set(index, nums[i]);
+            }
+        }
+        return sub.size();
     }
 }
