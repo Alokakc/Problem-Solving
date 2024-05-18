85249d46f3ded926d0383e2d17a9a894ceac0f1c
Alok Kumar <121576359+Alokakc@users.noreply.github.com>
2024-05-18 12:44:01 +0530

    Update Length_of_LS_That_Sums_to_Target.java

--- Online_TT_Class/Dynamic Programming-4/Length_of_LS_That_Sums_to_Target.java
+++ Online_TT_Class/Dynamic Programming-4/Length_of_LS_That_Sums_to_Target.java
@@ -1,22 +1,74 @@
-import java.util.*;
-
-//memo
 class Solution {
-    public int solve(List<Integer> nums, int i, int target, int[][] dp) {
-        if(target==0) return 0;
-        if(i>=nums.size() || target<0) return Integer.MIN_VALUE;
-        if(dp[i][target] != -1) return dp[i][target];
-        int ans = Integer.MIN_VALUE;
-        ans = Math.max(ans, solve(nums, i+1, target, dp));
-        int newans = Math.max(ans, 1+solve(nums, i+1, target-nums.get(i), dp));
-        return dp[i][target] = Math.max(ans, newans);
-    }
+
+    //MEMOIZATION
+
+    // public int solve(List<Integer> nums, int i, int t, int[][] dp) {
+    //     if(i<0) {
+    //         if(t==0) return 0;
+    //         else return Integer.MIN_VALUE;
+    //     }
+    //     if(t<0) return Integer.MIN_VALUE;
+    //     if(dp[i][t] != -1) return dp[i][t];
+    //     int pick = 1 + solve(nums, i-1, t-nums.get(i), dp);
+    //     int nonpick = solve(nums, i-1, t, dp);
+    //     return dp[i][t] = Math.max(pick, nonpick);
+    // }
+    // public int lengthOfLongestSubsequence(List<Integer> nums, int target) {
+    //     int n = nums.size();
+    //     int[][] dp = new int[n+1][target+1];
+    //     for(int[] x: dp) {
+    //         Arrays.fill(x, -1);
+    //     }
+    //     int ans = solve(nums, n-1, target, dp);
+    //     return ans<0?-1:ans;
+    // }
+
+    //TABULATION
+
+    // public int lengthOfLongestSubsequence(List<Integer> nums, int target) {
+    //     int n = nums.size();
+    //     int[][] dp = new int[n+1][target+1];
+    //     for (int i = 0; i <= n; i++) {
+    //         for (int t = 0; t <= target; t++) {
+    //             if (i == 0) {
+    //                 if (t == 0) {
+    //                     dp[i][t] = 0;
+    //                 } else {
+    //                     dp[i][t] = Integer.MIN_VALUE;
+    //                 }
+    //             } else {
+    //                 dp[i][t] = Integer.MIN_VALUE;
+    //             }
+    //         }
+    //     }
+    //     for(int i=1; i<=n; i++) {
+    //         for(int j=0; j<=target; j++) {
+    //             int pick = Integer.MIN_VALUE;
+    //             if(j-nums.get(i-1) >= 0) pick = 1 + dp[i-1][j-nums.get(i-1)];
+    //             int nonpick = dp[i-1][j];
+    //             dp[i][j] = Math.max(pick, nonpick);
+    //         }
+    //     }
+    //     return dp[n][target] < 0 ? -1 : dp[n][target];
+    // }
+
+    //SPACE-OPTIMIZATION
+
     public int lengthOfLongestSubsequence(List<Integer> nums, int target) {
-        int[][] dp = new int[nums.size()+1][target+2];
-        for(int[] x: dp) {
-            Arrays.fill(x, -1);
+        int n = nums.size();
+        int[] prev = new int[target+1];
+        int[] curr = new int[target+1];
+        Arrays.fill(prev, Integer.MIN_VALUE);
+        for(int i=1; i<=n; i++) {
+            prev[0] = 0;
+            for(int j=0; j<=target; j++) {
+                int pick = Integer.MIN_VALUE;
+                if(j-nums.get(i-1) >= 0) pick = 1 + prev[j-nums.get(i-1)];
+                int nonpick = prev[j];
+                curr[j] = Math.max(pick, nonpick);
+            }
+            prev = Arrays.copyOf(curr, target+1);
         }
-        int ans = solve(nums, 0, target, dp);
-        return ans<0?-1:ans;
+        return prev[target] < 0 ? -1 : prev[target];
     }
 }
